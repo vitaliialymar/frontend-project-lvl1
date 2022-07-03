81688a6a6f7d53d542b3dd7dd1069e027b6219d5
Vitaliia <vitaliialymar@gmail.com>
2022-07-04 00:03:48 +0300

    add brain-even

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/vitaliialymar/frontend-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/vitaliialymar/frontend-project-lvl1/actions)
 
 <a href="https://codeclimate.com/github/vitaliialymar/frontend-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/e2890269ea4bbd1094f2/maintainability" /></a>
+
+https://asciinema.org/a/UyB5VeidXndvk7oE6KvCPm4q4
--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,39 +1,38 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
+const game = () => {
+  console.log('Welcome to the Brain Games!');
 
-const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-console.log('Question: 15');
-const QuestionOne = () => {
-  if (readlineSync.keyInYN('Your answer: ')) {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let's try again, ${userName}!`);
-  } else {
-    console.log('Correct!');
+  const randomNumber = () => Math.floor(Math.random() * 100);
+
+  for (let i = 1; i <= 3; i += 1) {
+    const num = randomNumber();
+    console.log(`Question: ${num}`);
+    const answer = readlineSync.question('You answer: ');
+    if (num % 2 === 0) {
+      if (answer === 'yes') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+    }
+    if (num % 2 !== 0) {
+      if (answer === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.`);
+        console.log(`Let's try again, ${userName}!`);
+        return;
+      }
+    }
+    console.log(`Congratulations, ${userName}!`);
   }
 };
 
-console.log('Question: 6');
-const QuestionTwo = () => {
-  if (readlineSync.keyInYN('Your answer: ')) {
-    console.log('Correct!');
-  } else {
-    console.log("'no' is wrong answer ;(. Correct answer was 'yes'.");
-    console.log(`Let's try again, ${userName}!`);
-  }
-};
-
-console.log('Question: 7');
-const QuestionThree = () => {
-  if (readlineSync.keyInYN('Your answer: ')) {
-    console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-    console.log(`Let's try again, ${userName}!`);
-  } else {
-    console.log('Correct!');
-  }
-};
-
-console.log(`Congratulations, ${userName}!`)
+game();
